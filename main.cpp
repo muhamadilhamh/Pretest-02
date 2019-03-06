b150449c36bc9950bc9c9ba3a83b4fc93c770b11
muhamadilhamh <ilham.habib190600@gmail.com>
2019-03-06 10:39:27 +0700

    pretest-02

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/* Nama = Muhamad Ilham Habib
+   NPM  = 1408010180018
+   Kelas = B
+   Tanggal = 6 Maret 2019
+*/
+#include <iostream>
+
+using namespace std;
+
+void moveZeroToFront(int arr[], int n);
+void input(int (&arr)[100], int& n);
+void output(int arr[], int n);
+
+int main(){
+        int arr[100];
+        int n;
+        input(arr,n);
+        moveZeroToFront(arr,n);
+        output(arr,n)
+}
+void moveZeroToFront(int arr[], int n){
+        if i
+}
+void input(int (&arr)[100], int& n){
+    cout<<"masukan nilai n";cin>>n
+    for(int i=0;i<n;i++){
+        cout<<"masukan angka =";cin arr[i];
+    }
+}
+void output(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
